b7f7987bc995d3f67b42197303f801a820716350
chasetbass5 <chasetbass5@gmail.com>
2022-05-15 17:24:19 -0700

    Some basic levels
    Got a total of 8 levels. 4 are "easy" and then 4 are "hard" based of the easy levels.

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Stats
 {
-    private static float momentum = 15;
+    private static float momentum = 10;
     private static int stamina = 5;
     private static int difficulty = 1;
     private static int score = 0;
